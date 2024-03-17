79468f8e9105e9a6d25ddafa88be5bf3e10b9cc0
BaimoQilin <admin@baimoqilin.top>
2024-03-17 22:02:55 +0800

    feat: Force using gpt-4-turbo-preview if the user set the CODING_MODEL to gpt-4
    Because gpt-4 is not longer supports json modes.

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from log_writer import logger
 with open("config.yaml", "r") as conf:
     config_content = yaml.safe_load(conf)
     for key, value in config_content.items():
+        if key == "CODING_MODEL" and value == "gpt-4":
+            globals()[key] = "gpt-4-turbo-preview" # Force using gpt-4-turbo-preview if the user set the CODING_MODEL to gpt-4. Because gpt-4 is not longer supports json modes.
         globals()[key] = value
         logger(f"config: {key} -> {value}")
 
